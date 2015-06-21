ce065683b4d82099bbbf03bc9c2033abef2da26e
Tommy Jiang <tianyangj@gmail.com>
2015-06-20 21:50:40 -0700

    add parse, node, underscore typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,6 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+/// <reference path="parse/parse.d.ts" />

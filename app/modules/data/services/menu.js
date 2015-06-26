79cdf4999d9e975ed50bb70bec4c74b1763f0a33
Tommy Jiang <tianyangj@gmail.com>
2015-06-25 22:47:54 -0700

    refactor menuSvc

---
+++ app/modules/data/services/menu.js
@@ -0,0 +1,46 @@
+var lilybook;
+(function (lilybook) {
+    var data;
+    (function (data) {
+        'use strict';
+        var MenuSvc = (function () {
+            function MenuSvc($q) {
+                this.$q = $q;
+            }
+            ;
+            MenuSvc.prototype.getSideNav = function () {
+                var menu;
+                menu = [{
+                        subheader: 'Your Hub',
+                        items: [
+                            { icon: 'home', title: 'Home' },
+                            { icon: 'portrait', title: 'Profile' },
+                            { icon: 'group', title: 'Friends' },
+                            { icon: 'home', title: 'Loved' },
+                            { icon: 'home', title: 'History' }
+                        ]
+                    }, {
+                        subheader: 'Discovery',
+                        items: [
+                            { icon: 'home', title: 'Recommendations' },
+                            { icon: 'portrait', title: 'Composers' },
+                            { icon: 'group', title: 'Compositions' }
+                        ]
+                    }, {
+                        subheader: 'Management',
+                        items: [
+                            { icon: 'home', title: 'Login' },
+                            { icon: 'portrait', title: 'Signup' },
+                            { icon: 'group', title: 'Account' },
+                            { icon: 'group', title: 'Setting' }
+                        ]
+                    }];
+                return this.$q.when(menu);
+            };
+            MenuSvc.$inject = ['$q'];
+            return MenuSvc;
+        })();
+        data.MenuSvc = MenuSvc;
+        lilybook.data.module.service('menuSvc', MenuSvc);
+    })(data = lilybook.data || (lilybook.data = {}));
+})(lilybook || (lilybook = {}));
---
+++ app/modules/data/services/menu.ts
@@ -0,0 +1,56 @@
+module lilybook.data {
+	'use strict';
+
+	export interface ISideNavItem {
+		icon: string,
+		title: string
+	}
+
+	export interface ISideNavGroup {
+		subheader: string,
+		items: ISideNavItem[]
+	}
+
+	export interface IMenuSvc {
+		getSideNav(): ng.IPromise<ISideNavGroup[]>
+	}
+
+	export class MenuSvc implements IMenuSvc {
+
+		static $inject = ['$q'];
+
+		constructor(private $q: ng.IQService) { };
+
+		getSideNav() {
+			var menu: ISideNavGroup[];
+			menu = [{
+				subheader: 'Your Hub',
+				items: [
+					{ icon: 'home', title: 'Home' },
+					{ icon: 'portrait', title: 'Profile' },
+					{ icon: 'group', title: 'Friends' },
+					{ icon: 'home', title: 'Loved' },
+					{ icon: 'home', title: 'History' }
+				]
+			}, {
+					subheader: 'Discovery',
+					items: [
+						{ icon: 'home', title: 'Recommendations' },
+						{ icon: 'portrait', title: 'Composers' },
+						{ icon: 'group', title: 'Compositions' }
+					]
+				}, {
+					subheader: 'Management',
+					items: [
+						{ icon: 'home', title: 'Login' },
+						{ icon: 'portrait', title: 'Signup' },
+						{ icon: 'group', title: 'Account' },
+						{ icon: 'group', title: 'Setting' }
+					]
+				}];
+			return this.$q.when(menu);
+		}
+	}
+
+	lilybook.data.module.service('menuSvc', MenuSvc);
+}

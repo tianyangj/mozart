7d2170043a7406d1e1b5c0d13994d3f9973da154
Tommy Jiang <tianyangj@gmail.com>
2015-07-04 17:27:28 -0700

    update icon svg

--- app/modules/app/app.config.js
+++ app/modules/app/app.config.js
@@ -8,9 +8,9 @@ var lilybook;
                 .html5Mode(false)
                 .hashPrefix('!');
             $mdIconProvider
-                .icon("music_library", "./assets/svg/ic_library_music_black_48px.svg", 24)
-                .icon("arrow_right", "./assets/svg/ic_chevron_right_black_48px.svg", 24)
-                .icon("close", "./assets/svg/ic_close_black_48px.svg", 24);
+                .icon("music_library", "./assets/svg/ic_library_music.svg", 24)
+                .icon("arrow_right", "./assets/svg/ic_chevron_right.svg", 24)
+                .icon("close", "./assets/svg/ic_close.svg", 24);
             $mdThemingProvider
                 .theme('default')
                 .primaryPalette('blue')
